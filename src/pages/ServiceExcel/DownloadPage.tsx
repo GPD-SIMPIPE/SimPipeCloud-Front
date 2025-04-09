066f75892219bb187551bba93d5e538e19376cd5
Fernando Garcia <fernando.garcia@simpipe.com>
2025-04-09 15:24:50 -0300

    Update changes

--- src/pages/ServiceExcel/DownloadPage.tsx
+++ src/pages/ServiceExcel/DownloadPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import FileSaver from "file-saver";
 import Header from "../../components/Header/Header";
 import { useLocation, useNavigate } from "react-router-dom";
